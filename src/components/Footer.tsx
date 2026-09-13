7c8f241ff24e29822bc6d643ce9d8a5cb55a914a
unknown <you@example.com>
2026-09-13 22:06:03 +0600

    Updated Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,9 @@ const Footer = () => {
         <>
             <div className='mt-16 mb-12 mx-5 
         md:mx-18 px-5 md:px-8'>
-                <div className='flex justify-between pb-12'>
-                    <div>
-                        <img src={FooterLogo} className='mb-3 ' alt="" />
+                <div className='flex flex-col md:flex-row md:justify-between gap-8 md:gap-12 pb-10'>
+                    <div className='max-w-sm'>
+                        <img src={FooterLogo} className='mb-3 ' alt="Dev Stack" />
                         <p className='text-[12px] leading-4.5 text-[#64748B] mb-3 '>Curated tools, technologies, and resources for developers building
                             modern software.</p>
                         <ul className='text-[#475569] pt-3 flex gap-4'>
@@ -19,8 +19,8 @@ const Footer = () => {
 
 
                     <div>
-                        <h4 className='text-[#0F172A]'>PRODUCT</h4>
-                        <ul className='text-[#64748B]'>
+                        <h4 className='text-[#0F172A] font-semibold mb-3'>PRODUCT</h4>
+                        <ul className='text-[#64748B] space-y-2 text-sm'>
                             <li><a href="">Home</a></li>
                             <li><a href="">Technologies</a></li>
                             <li><a href="">Projects</a></li>
@@ -29,8 +29,8 @@ const Footer = () => {
 
 
                     <div>
-                        <h3 className='text-[#0F172A]'>COMPANY</h3>
-                        <ul className='text-[#64748B]'>
+                        <h4 className='text-[#0F172A] font-semibold mb-3'>COMPANY</h4>
+                        <ul className='text-[#64748B]  space-y-2 text-sm"'>
                             <li><a href="">About</a></li>
                             <li><a href="">Contact</a></li>
                             <li><a href="">Careers</a></li>
@@ -39,8 +39,8 @@ const Footer = () => {
 
 
                     <div>
-                        <h5 className='text-[#0F172A]'>LEGAL</h5>
-                        <ul className='text-[#64748B]'>
+                        <h4 className='text-[#0F172A] font-semibold mb-3'>LEGAL</h4>
+                        <ul className='text-[#64748B] space-y-2 text-sm'>
                             <li><a href="">Privacy Policy</a></li>
                             <li><a href="">Terms of Service</a></li>
                         </ul>
@@ -48,9 +48,9 @@ const Footer = () => {
                 </div>
 
 
-                <div className='flex justify-between text-[#94A3B8]'>
+                <div className='justify-between  border-t border-slate-800 pt-5 flex flex-col md:flex-row md:justify-between gap-3 text-[#94A3B8] text-xs'>
                     <p>© 2026 Dev Stack. All rights reserved</p>
-                    <div>
+                    <div className='flex gap-4'>
                         <a href="">Privacy</a>
                         <a href="">Terms</a>
                     </div>
